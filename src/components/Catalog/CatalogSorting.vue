d1adf8b25fabd174193476f106ed0da8cf2a2655
Rooseveltiny <45924428+Rooseveltiny@users.noreply.github.com>
2020-07-08 09:04:28 +0300

    starting to add sorting

--- src/components/Catalog/CatalogSorting.vue
+++ src/components/Catalog/CatalogSorting.vue
@@ -1,15 +1,30 @@
 <template>
-  
-<div>Сортировка</div>
-
+  <div class="sorting_inner">
+      Сортировать по: {{getCurrentSortingType}}
+    <div class="sorting_list">
+      <div class="sorting_item">По возрастанию цены</div>
+      <div class="sorting_item">По убыванию цены</div>
+      <div class="sorting_item">По наименованию</div>
+      <div class="sorting_item">По рейтингу</div>
+    </div>
+  </div>
 </template>
 
 <script>
+import {mapGetters} from 'vuex'
 export default {
-
-}
+    computed:{
+        ...mapGetters(['getCurrentSortingType'])
+    }
+};
 </script>
 
 <style>
-
+.sorting_list {
+  display: flex;
+  display: none;
+}
+.sorting_inner{
+    text-align: right;
+}
 </style>
--- src/store/modules/catalog.js
+++ src/store/modules/catalog.js
@@ -2,6 +2,17 @@
 export default {
     actions: {},
     mutation: {},
-    state: {},
-    getters: {}
+    state: {
+        catalog: {
+            sorting: {
+                sortingTypes: ['по возрастанию цены', "по убыванию цены", "по наименованию", "по рейтингу"],
+                currentSorting: 'по возрастанию цены'
+            }
+        }
+    },
+    getters: {
+        getCurrentSortingType(state) {
+            return state.catalog.sorting.currentSorting
+        }
+    }
 }
