2d66359e6ea9419e3b558271a1c3c4282f83d8c5
Rooseveltiny <45924428+Rooseveltiny@users.noreply.github.com>
2020-07-09 16:29:41 +0300

    Revert "Revert "product page many adds""
    This reverts commit 92688519b920d781dbd8aa57572520d7db96515a.

--- src/components/Product/ProductInfo.vue
+++ src/components/Product/ProductInfo.vue
@@ -1,15 +1,65 @@
 <template>
-  
-    <div>Information</div>
-
+  <div class="product_info_inner">
+    <div class="sections main_block_style main_block_style-less main_block_style-right_margin">
+      <div class="sections_inner">
+        <div
+          v-for="section in getAllSections"
+          :key="section"
+          class="section_item non-select"
+          :class="{'section_item-selected': section == getCurrentSection}"
+          @click="changeSection(section)"
+        >{{section}}</div>
+      </div>
+    </div>
+    <transition name="component-fade" mode="out-in">
+      <div class="section main_block_style main_block_style-less">
+        <ProductInfoInner />
+      </div>
+    </transition>
+  </div>
 </template>
 
 <script>
+import ProductInfoInner from "./ProductInfoInner";
+import { mapGetters, mapMutations } from "vuex";
 export default {
-
-}
+  components: {
+    ProductInfoInner
+  },
+  computed: {
+    ...mapGetters(["product", "getAllSections", "getCurrentSection"])
+  },
+  methods: {
+    ...mapMutations(["changeSection"])
+  }
+};
 </script>
 
-<style>
+<style scoped>
+.sections {
+  display: grid;
+  width: 20%;
+}
 
+.product_info_inner {
+  display: flex;
+}
+
+.section {
+  width: 80%;
+}
+
+.sections_inner {
+  padding: 0 10px;
+}
+
+.section_item {
+  padding: 5px;
+  font-size: 16px;
+  cursor: pointer;
+}
+
+.section_item-selected {
+  background-color: #ffe373;
+}
 </style>
