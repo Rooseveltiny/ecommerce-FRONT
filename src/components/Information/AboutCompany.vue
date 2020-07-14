0fd82ca7094da41946620c4aa9635a3f823b9721
Rooseveltiny <45924428+Rooseveltiny@users.noreply.github.com>
2020-07-14 14:33:48 +0300

    added content to about company

--- src/components/Information/AboutCompany.vue
+++ src/components/Information/AboutCompany.vue
@@ -6,8 +6,42 @@
                     О компании
                 </div>
             <div class="about_company_inner">
-            
+                <div class="about_company-info main_block_style main_block_style-less">
+                    <p>Компания "Всё для кровли" была основана в 2005 году в г. Череповце.
+                        Основным видом деятельности организации является обработка тонколистового проката давлением. 
+                        У предприятия имеется несколько производственных помещений с новейшим скоростным оборудованием,
+                        а также опытный штат сотрудников. 
+                    </p>
+                    <p>
+                        Основные виды продукции производимые компанией "Всё для кровли":
+                        <ul>
+                            <li>Профнастил</li>
+                            <li>Металлочерепица</li>
+                            <li>Металлический сайдинг</li>
+                            <li>Металлоштакетник</li>
+                            <li>Доборные элементы</li>
+                            <li>Металлические грядки</li>
+                        </ul>
+                    </p>
+                </div>
             </div>
+            <div class="main_title">
+                    Наши проекты
+                </div>
+                <div class="projects">
+                    <div class="project_item">
+                        Всё для кровли
+                    </div>
+                    <div class="project_item">
+                        Евроштакетник
+                    </div>
+                    <div class="project_item">
+                        Металлические грядки
+                    </div>
+                    <div class="project_item">
+                        Откатные ворота
+                    </div>
+                </div>
         </div>
     </div>
 
@@ -20,6 +54,20 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+
+.about_company-info{
+
+}
+
+.projects {
+    display: flex;
+}
+
+.project_item{
+    width: 25%;
+    text-align: center;
+    height: 100px;
+}
 
 </style>
